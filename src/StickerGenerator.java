3aef18dde32e0c9032bd51ef9af5265c820266cd
hyperGT <charlesgabriel102@gmail.com>
2023-03-28 18:18:08 -0300

    Sticker Generator class

---
+++ src/StickerGenerator.java
@@ -0,0 +1,68 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StickerGenerator {
+
+    public void createImage(InputStream inputStream, String nomeArquivo, String textoDaImagem, InputStream inputStreamSobreposicao) throws IOException {
+
+        // leitura da imagem
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria uma nova imagem transparente abaixo da imagem original (identificar altura x largura)
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        BufferedImage imagemSobreposicao = ImageIO.read(inputStreamSobreposicao);
+        int posicaoImagemSobreporY = novaAltura - imagemSobreposicao.getHeight();
+        graphics.drawImage(imagemSobreposicao, 0, posicaoImagemSobreporY, null);
+
+        //Configurar a fonte
+        var fonte = new Font("Impact", Font.BOLD, 40);
+        graphics.setColor(Color.ORANGE);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na parte transparente da imagem
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D retangulo = fontMetrics.getStringBounds(textoDaImagem, graphics);
+        int larguraTexto = (int) retangulo.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+        int posicaoTextoY = novaAltura - 100;
+        graphics.drawString(textoDaImagem, posicaoTextoX, posicaoTextoY);
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(textoDaImagem, fonte, fontRenderContext);
+
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, posicaoTextoY);
+        graphics.setTransform(transform);
+
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
+
+
+        // escrever a nova imagem num arquivo de saida
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+
+    }
+
+
+}
