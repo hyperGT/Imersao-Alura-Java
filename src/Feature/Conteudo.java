b4748753d3cfe638c7a07588d5b740ceecdb6ad9
hyperGT <charlesgabriel102@gmail.com>
2023-04-03 16:08:33 -0300

    contents record file

--- src/Feature/Conteudo.java
+++ src/Feature/Conteudo.java
@@ -1,30 +1,3 @@
 package Feature;
 
-public class Conteudo {
-
-    private String titulo;
-    private String urlImagem;
-
-    private String imdbRating;
-
-    public Conteudo(String imdbRating) {
-        this.imdbRating = imdbRating;
-    }
-
-    public Conteudo(String titulo, String urlImagem) {
-        this.titulo = titulo;
-        this.urlImagem = urlImagem;
-    }
-
-    public String getImdbRating() {
-        return imdbRating;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getUrlImagem() {
-        return urlImagem;
-    }
-}
+public record Conteudo (String titulo, String urlImagem) {}
